be8ca8beb52f622ff923d91662aa30f935309b3e
Ethan Christie <the.eejay@gmail.com>
2013-05-03 12:25:25 +1000

    Changed to default to course at uni
    Very temporary - will add in functionality to choose course from app

--- src/com/example/runspyrun/AttackActivity.java
+++ src/com/example/runspyrun/AttackActivity.java
@@ -98,7 +98,7 @@ public class AttackActivity extends Activity implements LocationListener, Archit
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
-		button = "Button1";
+		button = "Button2";
 		
 		if (!ArchitectView.isDeviceSupported(this)) {
 			Toast.makeText(this, "minimum requirements not fulfilled",
