472dbfe2c3f8a38c09cf89f7d14e0ce25c8a88cc
CDBuckmaster <callumbuckmaster@gmail.com>
2013-05-08 13:22:40 +1000

    Update MainActivity.java

--- src/com/example/runspyrun/MainActivity.java
+++ src/com/example/runspyrun/MainActivity.java
@@ -5,17 +5,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+    
+	private EditText longEdit;
+	private EditText latEdit;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        longEdit = (EditText)findViewById(R.id.longedit);
+        latEdit = (EditText)findViewById(R.id.latedit);
     }
     
     public void makeCourse(View view) {
     	Intent intent = new Intent(this, DefendActivity.class);
+    	intent.putExtra("longitude", Double.parseDouble(longEdit.getText().toString()));
+    	intent.putExtra("latitude", Double.parseDouble(latEdit.getText().toString()));
     	startActivity(intent);
     }
     
