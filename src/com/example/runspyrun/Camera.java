45b54a8240ee5eccc67ec1995e4882449b634582
CDBuckmaster <callumbuckmaster@gmail.com>
2013-06-06 13:05:02 +1000

    Took out project properties

---
+++ src/com/example/runspyrun/Camera.java
@@ -0,0 +1,13 @@
+package com.example.runspyrun;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Camera extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.camera);
+	
+	}
+}
